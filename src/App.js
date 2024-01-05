603ea598c28af699f677f517679d5b7dd037d0a0
Shabrez G <31725739+mohshabrez@users.noreply.github.com>
2024-01-05 12:08:53 +0530

    Update Main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { NavBar } from './components/NavBar';
+import { StudentView } from './components/studentView';
+import { StudentForm } from './components/StudentForm';
+import { StudentDetail } from './components/StudentDetail';
+import { TeacherView } from './components/TeacherView';
+import { TeacherForm } from './components/TeacherForm';
+import { TeacherDetail } from './components/TeacherDetail';
+import { SchoolView } from './components/SchoolView';
+import { ClassView } from './components/ClassView';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-gray-800 min-h-screen">
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<StudentView/>}/>
+        <Route path='/students/:id' element={<StudentDetail/>}/>
+        <Route path='/students/add' element={<StudentForm/>}/>
+        <Route path='/students/edit/:id' element={<StudentForm/>}/>
+        <Route path='/teachers' element={<TeacherView/>}/>
+        <Route path='/teachers/:id' element={<TeacherDetail/>}/>
+        <Route path='/teachers/add' element={<TeacherForm/>}/>
+        <Route path='/teachers/edit/:id' element={<TeacherForm/>}/>
+        <Route path='/schools' element={<SchoolView/>}/>
+        <Route path='/classes' element={<ClassView/>}/>
+      </Routes>
     </div>
   );
 }
